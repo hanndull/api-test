f7212e9f28d67031416e75f1c08602382f768579
Hannah Johnson <hstutzman@gmail.com>
2019-08-15 01:41:56 -0700

    renamed adyen file, added in payment method app route

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, request, jsonify
+from adyen_api import ady
 
 app = Flask(__name__) #Instanciate a Flask app object
 
@@ -8,6 +9,22 @@ app.secret_key = "Test" #Set the object's secret_key attribute
 def confirm_app():
 
     return "Hello"
+
+@app.route('/paymentMethods', methods = ['POST'])
+def get_avail_methods():
+    '''get a list of payment methods available to your shopper''' 
+    
+    result = ady.checkout.payment_methods({
+        'merchantAccount': 'YOUR_MERCHANT_ACCOUNT',
+        'countryCode': 'NL',
+        'amount': {
+                'value': 1000,
+                'currency': 'EUR'
+        },
+        'channel': 'Web',
+    })
+
+    return result
 
 
 if __name__ == "__main__":
